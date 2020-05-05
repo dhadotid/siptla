bbb04048a49534d910309c33972d746b722615f2
Fathan Qoriba <fathan.qoriba95@gmail.com>
2020-05-05 10:37:52 +0700

    modified data lhp on senior

--- resources/views/backend/pages/data-lhp/auditor-senior/data.blade.php
+++ resources/views/backend/pages/data-lhp/auditor-senior/data.blade.php
@@ -83,13 +83,13 @@
                                         <li>
                                             <a href="#" data-toggle="modal" data-target="#modalreview" data-value="{{$item->lhp_id}}" class="btn-review"><i class="glyphicon glyphicon-comment"></i> &nbsp;&nbsp;Tanggapan dan Review LHP</a>
                                         </li>
-                                        @if (Auth::user()->level=='super-user')  
+                                        {{-- @if (Auth::user()->level=='super-user')   --}}
                                             @if ($item->status_lhp!='Publish LHP') 
                                                 <li>
                                                     <a href="#" data-toggle="modal" data-target="#modaladdreview" data-value="{{$item->lhp_id}}" class="btn-add-review"><i class="fa fa-plus-circle"></i> &nbsp;&nbsp;Tambah Tanggapan dan Review LHP</a>
                                                 </li>
                                             @endif    
-                                        @endif
+                                        {{-- @endif --}}
                                         <li>
                                             <a href="#" class="btn-edit" data-toggle="modal" data-target="#modalubah" data-value="{{$item->lhp_id}}"><i class="glyphicon glyphicon-edit"></i> &nbsp;&nbsp;Edit LHP</a>
                                         </li>
@@ -167,16 +167,13 @@
                                     <li>
                                         <a href="#" data-toggle="modal" data-target="#modalreview" data-value="{{$item->lhp_id}}" class="btn-review"><i class="glyphicon glyphicon-comment"></i> &nbsp;&nbsp;Tanggapan dan Review LHP</a>
                                     </li>
-                                    @if (Auth::user()->level=='super-user')  
+                                    {{-- @if (Auth::user()->level=='super-user')   --}}
                                         @if ($item->status_lhp!='Publish LHP') 
                                             <li>
                                                 <a href="#" data-toggle="modal" data-target="#modaladdreview" data-value="{{$item->lhp_id}}" class="btn-add-review"><i class="fa fa-plus-circle"></i> &nbsp;&nbsp;Tambah Tanggapan dan Review LHP</a>
                                             </li>
-                                            <li>
-                                                <a href="javascript:publishlhp({{$item->lhp_id}})"><i class="glyphicon glyphicon-check"></i> &nbsp;&nbsp;Publish LHP</a>
-                                            </li>
                                         @endif    
-                                    @endif
+                                    {{-- @endif --}}
                                     <li>
                                         <a href="#" class="btn-edit" data-toggle="modal" data-target="#modalubah" data-value="{{$item->lhp_id}}"><i class="glyphicon glyphicon-edit"></i> &nbsp;&nbsp;Edit LHP</a>
                                     </li>
